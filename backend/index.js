d5d06ef40554144adcbdba21f55290c09f349195
Shijin P <shijinp006@gmail.com>
2026-03-16 11:22:30 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 
 app.use(
     cors({
-        origin: "http://localhost:3000", // frontend URL
+        origin: "*", // frontend URL
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     })
